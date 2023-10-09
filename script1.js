06abbd44b16fb498c2cdf39ca9ab0bae83d121a8
Nikita <nikitarawanisdl@gmail.com>
2023-10-09 18:20:06 +0530

    Initial commit

---
+++ script1.js
@@ -0,0 +1,8 @@
+let invest_now = document.querySelector('.div1 button');
+invest_now.addEventListener('click', investApp);
+
+function investApp(){
+    window.location.href = "https://groww.in/";
+    
+}
+
